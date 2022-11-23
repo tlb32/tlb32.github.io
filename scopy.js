6db47968f677d51978184cfc1f35d2108a268ad0
Todd Brooks <74124881+tlb32@users.noreply.github.com>
2022-11-23 12:45:21 +0000

    again

--- scopy.js
+++ scopy.js
@@ -1,5 +1,8 @@
 let copyText = document.querySelector(".copy-text");
 copyText.querySelector("button").addEventListener("click", function () {
+
+
+
     let input = copyText.querySelector("input.text");
     input.select();
     document.execCommand("copy");
@@ -9,3 +12,20 @@ copyText.querySelector("button").addEventListener("click", function () {
         copyText.classList.remove("active");
     }, 2500);
 });
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
+  let value = params.some_key; // "some_value"
